33e0453a8e17ec4c8ed4c5f0914a01c997ffa7b5
Akhmad Khanif Zyen <khanif.zyen@gmail.com>
2023-11-21 15:29:34 +0700

    auth logic

--- lib/auth.dart
+++ lib/auth.dart
@@ -3,4 +3,30 @@ import 'package:appwrite/appwrite.dart';
 Client client = Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject('655b107b0038b2778684')
-    .setSelfSigned(status: true); 
+    .setSelfSigned(status: true);
+
+//Register a user
+Account account = Account(client);
+Future<String> createUser(String name, String email, String password) async {
+  try {
+    final user = account.create(
+      email: email,
+      name: name,
+      password: password,
+      userId: ID.unique(),
+    );
+    return "success";
+  } on AppwriteException catch (e) {
+    return e.message.toString();
+  }
+}
+
+//Login user
+Future loginUser(String email, String password) async {
+  try {
+    final user = account.createEmailSession(
+      email: email,
+      password: password,
+    );
+  } catch (e) {}
+}
